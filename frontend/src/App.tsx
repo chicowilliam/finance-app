e616bd11534d50e3fb680f248f6f0bceca42ecb1
chicowilliam <viniciuswilliam91@gmail.com>
2026-04-16 12:47:35 -0300

    refactor(app): integrate DashboardLayout with nested routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import DashboardLayout from './layouts/DashboardLayout'
 import VisaoGeral from './pages/VisaoGeral'
 import Contas from './pages/Contas'
 import Calendario from './pages/Calendario'
@@ -8,16 +8,13 @@ import './App.css'
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <div className="page-wrapper">
-        <Routes>
-          <Route path="/"           element={<VisaoGeral />} />
-          <Route path="/contas"     element={<Contas />} />
-          <Route path="/calendario" element={<Calendario />} />
-          <Route path="/alertas"    element={<Alertas />} />
-        </Routes>
-      </div>
-    </>
+    <Routes>
+      <Route element={<DashboardLayout />}>
+        <Route path="/"           element={<VisaoGeral />} />
+        <Route path="/contas"     element={<Contas />} />
+        <Route path="/calendario" element={<Calendario />} />
+        <Route path="/alertas"    element={<Alertas />} />
+      </Route>
+    </Routes>
   )
 }
